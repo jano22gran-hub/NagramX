a39455c5f01a1961536b362b802416e53d9209dc
jano22gran-hub <jano22gran@gmail.com>
2026-03-23 06:49:03 +0300

    feat: add SessionBackupUI.java [NagramX]

---
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/ui/SessionBackupUI.java
@@ -0,0 +1,29 @@
+package tw.nekomimi.nekogram.ui;
+
+import android.app.Activity;
+import android.net.Uri;
+import org.telegram.messenger.LocaleController;
+import org.telegram.ui.ActionBar.AlertDialog;
+import tw.nekomimi.nekogram.helpers.SessionBackupHelper;
+
+public class SessionBackupUI {
+
+    public static void showBackupDialog(Activity activity) {
+        AlertDialog.Builder b = new AlertDialog.Builder(activity);
+        b.setTitle(LocaleController.getString("SessionBackup", 
+            tw.nekomimi.nekogram.R.string.SessionBackup));
+        b.setMessage(LocaleController.getString("SessionBackup_desc",
+            tw.nekomimi.nekogram.R.string.SessionBackup_desc));
+        b.setPositiveButton(LocaleController.getString("OK",
+            org.telegram.messenger.R.string.OK), (d, w) -> {
+            SessionBackupHelper.pickFolder(activity);
+        });
+        b.setNegativeButton(LocaleController.getString("Cancel",
+            org.telegram.messenger.R.string.Cancel), null);
+        b.show();
+    }
+
+    public static void handleBackupResult(Activity activity, Uri uri) {
+        SessionBackupHelper.handleBackupResult(activity, uri);
+    }
+}
